b4f8b48cc6a4e79fa9bd1c5a58d8ba6309c215d2
Colnuwko <Vladislav290403@gmail.com>
2022-10-27 12:34:54 +0400

    pre_worker

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def main():
     while number_elem < 50:
         response = requests.get(url, headers)  # Собственно записываем в переменную нтмл текст.после используем его
         response.encoding = "utf-8"
-        #   print(response.text)
+        print(response.text)
         soup = BeautifulSoup(response.text, 'lxml')
         items = soup.find_all(class_="footer-card__link", href=True)
         href = []
@@ -41,7 +41,7 @@ def main():
             href.append(item.get('href'))
         print(href)
         # Заходим на страницу и парсим все ссылки на полные отзывы.
-        # time.sleep(random.randint(10, 25))
+        time.sleep(random.randint(10, 20))
         for i in range(len(href)):
             sec_response = requests.get(second_url + href[i])
             sec_response.encoding = "utf-8"
@@ -57,45 +57,53 @@ def main():
             # print(str(len(new_quotes)) + " " + str(len(names))+" "+str(len(texts))+" "+str(len(authors)))
             # количество найденных данных Используется не количество найденных ссылок а мин значение из найденых
             # элементов, так ка бывает что отзыв без оценки или автора..
-            time.sleep(random.randint(30, 45))
-            y = min(len(authors), len(names), len(texts), new_quotes)
-            if i > y: continue
-            print(i, y, len(authors), len(names), len(texts))
-            x = float(quotes[i])
+            y = min(len(authors), len(names), len(texts))
+            print(i, y, len(authors), len(names), len(texts), new_quotes)
+            x = float(new_quotes[0])
             # Сортируем отзывы по оценкам и записываем в txt файл, W+ запись и чтение(модификатор)
             if 4.5 <= x <= 5:
                 quotes_5 = quotes_5 + 1
-                if quotes_5 >= 1000: continue
+                if quotes_5 >= 1000:
+                    continue
                 namefile = str(quotes_5).zfill(4)
                 with open("dataset/" + "5" + '/' + namefile + '.txt', 'w+', encoding="utf-8") as file:
-                    file.write(str(x) + '\n' + names + '\n' + authors + '\n' + texts)
+                    file.write('Оценка: ' + str(x) + '\n' + 'Название: ' + names[0].text + '\n' + 'Автор книги: ' +
+                               authors[0].text + '\n' + 'Рецензия:' + '\n' + texts[0].text)
             elif 3.5 <= x < 4.5:
                 quotes_4 = quotes_4 + 1
-                if quotes_4 >= 1000: continue
+                if quotes_4 >= 1000:
+                    continue
                 namefile = str(quotes_4).zfill(4)
                 with open("dataset/" + "4" + '/' + namefile + '.txt', 'w+', encoding="utf-8") as file:
-                    file.write(str(x) + '\n' + names + '\n' + authors + '\n' + texts)
+                    file.write('Оценка: ' + str(x) + '\n' + 'Название: ' + names[0].text + '\n' + 'Автор книги: ' +
+                               authors[0].text + '\n' + 'Рецензия:' + '\n' + texts[0].text)
             elif 2.5 <= x < 3.5:
                 quotes_3 = quotes_3 + 1
-                if quotes_3 >= 1000: continue
+                if quotes_3 >= 1000:
+                    continue
                 namefile = str(quotes_3).zfill(4)
                 with open("dataset/" + "3" + '/' + namefile + '.txt', 'w+', encoding="utf-8") as file:
-                    file.write(str(x) + '\n' + names + '\n' + authors + '\n' + texts)
+                    file.write('Оценка: ' + str(x) + '\n' + 'Название: ' + names[0].text + '\n' + 'Автор книги: ' +
+                               authors[0].text + '\n' + 'Рецензия:' + '\n' + texts[0].text)
             elif 1.5 <= x < 2.5:
                 quotes_2 = quotes_2 + 1
-                if quotes_2 >= 1000: continue
+                if quotes_2 >= 1000:
+                    continue
                 namefile = str(quotes_2).zfill(4)
                 with open("dataset/" + "2" + '/' + namefile + '.txt', 'w+', encoding="utf-8") as file:
-                    file.write(str(x) + '\n' + names + '\n' + authors + '\n' + texts)
+                    file.write('Оценка: ' + str(x) + '\n' + 'Название: ' + names[0].text + '\n' + 'Автор книги: ' +
+                               authors[0].text + '\n' + 'Рецензия:' + '\n' + texts[0].text)
             else:
                 quotes_1 = quotes_1 + 1
-                if quotes_1 >= 1000: continue
+                if quotes_1 >= 1000:
+                    continue
                 namefile = str(quotes_1).zfill(4)
                 with open("dataset/" + "1" + '/' + namefile + '.txt', 'w+', encoding="utf-8") as file:
-                    file.write(str(x) + '\n' + names + '\n' + authors + '\n' + texts)
+                    file.write('Оценка: ' + str(x) + '\n' + 'Название: ' + names[0].text + '\n' + 'Автор книги: ' +
+                               authors[0].text + '\n' + 'Рецензия:' + '\n' + texts[0].text)
             number_elem = number_elem + 1
+            time.sleep(random.randint(35, 40))
             # прибавляем счетчик
-            # time.sleep(random.randint(30, 45))
             # #режим ждуна шоб не забанило
         number_page = number_page + 1  # Инкрементируем счетчик страниц и перелистываем страничку, после все заново
         url = 'https://www.livelib.ru/reviews' + '~' + str(number_page) + '#reviews'
